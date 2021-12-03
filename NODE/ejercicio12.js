0c87c298d78ff776feca8ec55b4162bc6f9681ae
EBUENON <ebuenon44@alumnes.santjosepobrer.es>
2021-12-03 11:52:09 +0100

    EJERCICIO 12 CREADO

---
+++ NODE/ejercicio12.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/mayoromenor/', (req, res) => {
+    const a = Number(req.query.primero)
+    const b = Number(req.query.segundo)
+    if (isNaN(mayoromenor)) {
+        resultado = (a > b) ? `El mayor es ${a}`
+        resultado = (a < b) ? `El mayor es ${b}`
+    }
+
+    else (isNaN(mayoromenor)) {
+        resultado = (a = b) ? `Son iguales`
+    }
+    
+    res.send(resultado)
+})
+
+app.all('/', (req, res) => {
+    res.send(`<p>Puedes ir a la siguiente dirección:
+              <br>localhost:5000/mayoromenor/?primero=5&segundo=9
+              <br><br> Cambiar valores y probar funcionamiento
+            `)
+});
+
+app.listen(5000, () => console.log('Server ready on localhost:5000'))
